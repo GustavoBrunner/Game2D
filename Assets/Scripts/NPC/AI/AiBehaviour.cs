b0bffcff0d7590ea0a27a53bec3169e5074c6e6a
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-07-12 11:55:43 -0300

    ai restarted

--- Assets/Scripts/NPC/AI/AiBehaviour.cs
+++ Assets/Scripts/NPC/AI/AiBehaviour.cs
@@ -5,32 +5,17 @@ namespace Npc
     [CreateAssetMenu(fileName = "AiBehaviour", menuName = "NPC/AiBehaviour", order = 1)]
     public class AiBehaviour : ScriptableObject
     {
-        public int Id;
-        public NpcState State;
 
-        public float Speed = 5.0f;
-        public string Name;
-        public string ActualSceneName;
-        public string NextSceneName;
+        /*
+         * A IA dos npcs será baseada em estados, e cada estado mudará de acordo com o tempo atual do dia. Os dias
+         * serão guiados por hora e minutos.
+            Npc terá alguns comportamentos, como:
+            - Idle: Ficar parado, olhando para o nada, trocando a animação dele de tempos em tempos.
+            - Roam: o npc terá rotas pré-definidas, e irá andar por ela. Ao chegar no final de uma cena
+            e ainda não ter terminado sua rota, ele será teleportado para outra cena, onde continuará sua rota.
+            - Follow: o npc irá seguir o player, mas não irá atacar ele. Ele irá apenas seguir o player.
+            - GoSleep: o npc irá dormir, e acordará após um tempo. Ele irá trocar a animação dele de tempos em tempos.
 
-        public string Description;
-        public Sprite Icon;
-        public Sprite NpcSprite;
-
-        public int Day;
-        public int Hour;
-        public int Minute;
-
-        public bool IsActive = true;
-
-        
-
-
-
-
-        public AiBehaviour()
-        {
-            this.Id = (int)Random.Range(1,1000000) * 100;
-        }
+         */
     }
 }
--- Assets/Scripts/NPC/AI/Node.cs
+++ Assets/Scripts/NPC/AI/Node.cs
@@ -4,16 +4,7 @@ namespace Npc
 {
     public class Node : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
