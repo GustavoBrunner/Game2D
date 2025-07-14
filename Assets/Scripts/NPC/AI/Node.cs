f6819154affe6feb0c9df5c9e341a452eb137b8e
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-07-14 15:23:39 -0300

    gizmos nodes

--- Assets/Scripts/NPC/AI/Node.cs
+++ Assets/Scripts/NPC/AI/Node.cs
@@ -1,10 +1,19 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Npc
 {
     public class Node : MonoBehaviour
     {
-        
+        [SerializeField] private float m_Radius = 2f;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, m_Radius);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, 0.1f);
+        }
 
     }
 }
